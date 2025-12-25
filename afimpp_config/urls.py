02bf2ee2a5916a8ef9c401bd85c7fb2093812196
kennedy-ak <akogokennedy@gmail.com>
2025-12-25 15:07:45 +0000

    Update static and media file settings, enhance slideshow animation, and clean up unused assets

--- afimpp_config/urls.py
+++ afimpp_config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,12 +27,13 @@ urlpatterns = [
     path('courses/', include('courses.urls')),
     path('users/', include('users.urls')),
     path('contact/', include('contact.urls')),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico', permanent=True)),
 ]
 
-# Serve media files in development
+# Serve static and media files in development
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Admin site customization
 admin.site.site_header = "AfIMMP Administration"
